6fc04777724fbd7c47d3f71b4c8a501b5869f888
zhenzhencai <474760615@qq.com>
2018-05-27 18:14:09 +0800

    add express

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,13 @@ export default {
   name: 'app'
 }
 </script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+@import "src/styles/mixin.scss";
+#app{
+
+  // // background-color: #f3f3f3;
+  // // background-color: rgba(64, 64, 64, 0.5);
+  // background-image: url(../static/pumpkin.png);
+}
+</style>
